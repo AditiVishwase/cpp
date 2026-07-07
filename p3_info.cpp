0059bf251915f6846d7f8dab78d28af39e02b6f4
AditiVishwase <aditivishwase1107@gmail.com>
2026-07-07 20:18:26 +0530

    Add files via upload

---
+++ p3_info.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    cout<<"Aditi Vishwase"<<endl;
+    cout<<"sy btech cse"<<endl;
+    cout<<"roll no 327"<<endl;
+    return 0;
+    
+}
