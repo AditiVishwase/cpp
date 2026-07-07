6961bac6ca004a50b7421202222fe891a42f1741
AditiVishwase <aditivishwase1107@gmail.com>
2026-07-07 20:27:21 +0530

    Add files via upload

---
+++ p4_datatypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=19;
+    float height=5.2;
+    char grade='A';
+    string name="Aditi";
+    cout<<"Name:"<<name<<endl;
+    cout<<"age:"<<age<<endl;
+    cout<<"height:"<<height<<endl;
+    cout<<"grade:"<<grade<<endl;
+    return 0;    
+}
