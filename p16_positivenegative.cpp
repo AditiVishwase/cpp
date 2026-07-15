84d127cc034df9d633dee985a2cda3f9a86aa89b
AditiVishwase <aditivishwase1107@gmail.com>
2026-07-15 20:09:55 +0530

    Add files via upload

---
+++ p16_positivenegative.cpp
@@ -0,0 +1,15 @@
+//checking the given  number is positive or negative 
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number:"<<endl;
+    cin>>num;
+    if(num>0){
+        cout<<"the number is positive."<<endl;
+    }
+    else{
+        cout<<"the number is negative."<<endl;
+    }
+    return 0;
+}
