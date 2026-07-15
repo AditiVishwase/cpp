a22281bb9b200cdd06711a1eaf4f54a21b379d7a
AditiVishwase <aditivishwase1107@gmail.com>
2026-07-15 20:01:18 +0530

    Add files via upload

---
+++ p13_positive.cpp
@@ -0,0 +1,13 @@
+//check the number is positive or not
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the number:"<<endl;
+    cin>>num;
+    if(num>0){
+        cout<<"positive"<<endl;
+
+    }
+    return 0;
+}
---
+++ p14_votingeligibility.cpp
@@ -0,0 +1,13 @@
+//checking the voting eligibility of  the student
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"enter the age of  the person:"<<endl;
+    cin>>age;
+    if(age>=18){
+        cout<<"eligible"<<endl;
+    }
+    return 0;
+
+}
