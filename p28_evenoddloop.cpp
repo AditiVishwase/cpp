3cab69c72fcd56068dd677c2c5f0a3fd049d16b1
AditiVishwase <aditivishwase1107@gmail.com>
2026-07-18 22:00:39 +0530

    Add files via upload

---
+++ p28_evenoddloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter the value of num:"<<endl;
+    cin>>num;
+    for(int i=0;i<=num;i++){
+        if(i%2==0){
+            cout<<i<<endl;
+            
+        }
+    }
+    
+    return 0;
+}
